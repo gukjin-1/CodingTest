7c5acc982a92194c9a75190df653fd9f21f52080
gukjin-1 <droplet__@likelion.org>
2022-05-05 21:29:23 +0900

    codingtest

---
+++ 20220505.py
@@ -0,0 +1,19 @@
+import sys
+
+A, B = map(int,sys.stdin.readline().split())
+
+count = 1
+while True:
+    if A == B:
+        break
+    elif A > B or (B % 10 != 1 and B % 2 != 0):
+        count = -1
+        break
+    elif B % 10 == 1:
+        B //= 10
+        count+=1
+    elif B % 2 == 0:
+        B //= 2
+        count+=1
+    
+print(count)
