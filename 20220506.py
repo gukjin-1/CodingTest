584962a875c2a90d79b8672bd804434cb99fe6b1
gukjin-1 <droplet__@likelion.org>
2022-05-06 22:01:51 +0900

    codingtest

---
+++ 20220506.py
@@ -0,0 +1,23 @@
+n = int(input())
+k = int(input())
+sensor = list(map(int, input().split()))
+
+if k >= n:
+    print(0)
+else: 
+    sensor.sort()
+    gap = []
+    for i in range(1, n):
+        gap.append((sensor[i]-sensor[i-1], i))
+    gap.sort()
+
+    standard = [0]
+    result = 0
+    for i in range(k-1):
+        standard.append(gap.pop()[1])
+    standard.append(0)
+
+    result = 0
+    for i in range(k): 
+        result += sensor[standard[i+1]-1]-sensor[standard[i]]
+    print(result)
